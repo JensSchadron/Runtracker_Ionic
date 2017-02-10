2e2f7f3418a80844bde57a6f2acea7b46b4d01f8
Jens Schadron <jensschadron@hotmail.com>
2017-02-10 15:19:38 +0100

    Toevoegen tests + onnodige code in commentaar zetten

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,7 +1,6 @@
 import { browser, element, by } from 'protractor';
 
-describe('MyApp', () => {
-
+describe('Runtracker_Ionic login page', () => {
   beforeEach(() => {
     browser.get('');
   });
@@ -9,4 +8,27 @@ describe('MyApp', () => {
   it('should have a title', () => {
     expect(browser.getTitle()).toEqual('Login');
   });
+
+  it('should have navbar', () => {
+    let navBar = element(by.tagName('ion-navbar'));
+
+    expect(navBar.isPresent()).toBeTruthy();
+  });
+
+  it('test navbar title', () => {
+    let navBarTitle = element(by.tagName('ion-title'));
+
+    expect(navBarTitle.isPresent()).toBeTruthy();
+    expect(navBarTitle.getText()).toBe('Login');
+  });
+
+  it('should have login button', () => {
+    let btnLogin = element(by.id('btn-login'));
+
+    expect(btnLogin.isPresent()).toBeTruthy();
+    expect(btnLogin.getText()).toBe('LOGIN');
+    btnLogin.click();
+  });
+
+
 });
