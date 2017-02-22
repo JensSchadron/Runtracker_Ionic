4b02ae4f2abdfc17b7de41430ff248644e8f4cbc
stijnergeerts <KdG1p7PN>
2017-02-22 17:57:34 +0100

    Edit profile page commit 2

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -1,15 +1,29 @@
 import {Component} from "@angular/core";
 import {NavController, AlertController} from "ionic-angular";
+import {EditprofilePage} from '../editprofile/editprofile'
+
+import {EditProfileService} from '../editprofile/editprofile.service';
+import {AuthService} from '../../services/auth/auth.service';
+import {User} from "../../app/model/user";
+
 /**
  * Created by stijnergeerts on 20/02/17.
  */
 @Component({
   selector: 'page-profile',
-  templateUrl: 'profile.html'
+  templateUrl: 'profile.html',
+  providers: [EditProfileService]
 })
 export class ProfilePage {
+  private user;
+  editProfilePage: any = EditprofilePage;
 
-  constructor(public navCtrl: NavController, private alertCtrl: AlertController) {
+  ngOnInit(): void {
+    this.user = this.editProfileService.getUser().subscribe((user: User) => this.user = user);
+
+  }
+
+  constructor(public navCtrl: NavController, private alertCtrl: AlertController, private editProfileService: EditProfileService, private auth: AuthService) {
 
   }
 
