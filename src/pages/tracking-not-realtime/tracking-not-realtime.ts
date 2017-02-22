40bf253d9e41a043eaef7a3dd82a11d52f7b641e
Alexander van Ravestyn <alexander.vanravestyn@student.kdg.be>
2017-02-22 11:02:13 +0100

    Bijwerken Tracking-not-realtime

--- src/pages/tracking-not-realtime/tracking-not-realtime.ts
+++ src/pages/tracking-not-realtime/tracking-not-realtime.ts
@@ -1,16 +1,16 @@
 import { Component, ViewChild, ElementRef } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import { NavController, NavParams, LoadingController } from 'ionic-angular';
 import { Geolocation } from 'ionic-native';
 
 
 declare var google;
 
 /*
-  Generated class for the TrackingNotRealtime page.
+ Generated class for the TrackingNotRealtime page.
 
-  See http://ionicframework.com/docs/v2/components/#navigation for more info on
-  Ionic pages and navigation.
-*/
+ See http://ionicframework.com/docs/v2/components/#navigation for more info on
+ Ionic pages and navigation.
+ */
 @Component({
   selector: 'page-tracking-not-realtime',
   templateUrl: 'tracking-not-realtime.html'
@@ -19,15 +19,24 @@ declare var google;
 export class TrackingNotRealtimePage {
   @ViewChild('map') mapElement: ElementRef;
   map: any;
+  loadingPopup: any;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {}
+  constructor(public navCtrl: NavController, public navParams: NavParams, private loadingCtrl: LoadingController) {
+
+    this.loadingPopup = this.loadingCtrl.create({
+      content: 'Fetching location...'
+    });
+
+  }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad TrackingNotRealtimePage');
     this.loadMap();
   }
 
-  loadMap(){
+  loadMap() {
+
+    this.loadingPopup.present();
 
     Geolocation.getCurrentPosition().then((position) => {
 
@@ -35,14 +44,138 @@ export class TrackingNotRealtimePage {
 
       let mapOptions = {
         center: latLng,
-        zoom: 17
-        ,
-        mapTypeId: google.maps.MapTypeId.ROADMAP
+        zoom: 17,
+        scrollwheel: false,
+        disableDefaultUI: true,
+        mapTypeId: google.maps.MapTypeId.ROADMAP,
+        styles: [{
+          "featureType": "administrative",
+          "elementType": "labels.text.fill",
+          "stylers": [{"color": "#444444"}]
+        }, {
+          "featureType": "administrative.country",
+          "elementType": "labels.text",
+          "stylers": [{"color": "#003f5f"}]
+        }, {
+          "featureType": "administrative.province",
+          "elementType": "labels.text",
+          "stylers": [{"color": "#003f5f"}]
+        }, {
+          "featureType": "administrative.locality",
+          "elementType": "labels.text.fill",
+          "stylers": [{"color": "#003f5f"}]
+        }, {
+          "featureType": "administrative.neighborhood",
+          "elementType": "labels.text.fill",
+          "stylers": [{"color": "#003f5f"}]
+        }, {
+          "featureType": "administrative.land_parcel",
+          "elementType": "labels.text.fill",
+          "stylers": [{"color": "#003f5f"}]
+        }, {
+          "featureType": "landscape",
+          "elementType": "all",
+          "stylers": [{"color": "#f2f2f2"}]
+        }, {
+          "featureType": "landscape",
+          "elementType": "geometry.fill",
+          "stylers": [{"color": "#c1b8af"}, {"lightness": "69"}]
+        }, {
+          "featureType": "landscape.man_made",
+          "elementType": "labels.text.fill",
+          "stylers": [{"color": "#003f5f"}]
+        }, {
+          "featureType": "landscape.natural.landcover",
+          "elementType": "labels.text.fill",
+          "stylers": [{"color": "#003f5f"}]
+        }, {
+          "featureType": "landscape.natural.terrain",
+          "elementType": "labels.text.fill",
+          "stylers": [{"color": "#003f5f"}]
+        }, {
+          "featureType": "poi",
+          "elementType": "all",
+          "stylers": [{"visibility": "off"}]
+        }, {
+          "featureType": "poi.attraction",
+          "elementType": "labels.text.fill",
+          "stylers": [{"color": "#003f5f"}]
+        }, {
+          "featureType": "poi.business",
+          "elementType": "labels.text.fill",
+          "stylers": [{"color": "#003f5f"}]
+        }, {
+          "featureType": "poi.government",
+          "elementType": "labels.text.fill",
+          "stylers": [{"color": "#003f5f"}]
+        }, {
+          "featureType": "poi.medical",
+          "elementType": "labels.text.fill",
+          "stylers": [{"color": "#003f5f"}]
+        }, {
+          "featureType": "poi.park",
+          "elementType": "geometry.fill",
+          "stylers": [{"color": "#978b82"}]
+        }, {
+          "featureType": "poi.park",
+          "elementType": "labels.text",
+          "stylers": [{"color": "#003f5f"}]
+        }, {
+          "featureType": "poi.place_of_worship",
+          "elementType": "labels.text.fill",
+          "stylers": [{"color": "#003f5f"}]
+        }, {
+          "featureType": "poi.school",
+          "elementType": "labels.text.fill",
+          "stylers": [{"color": "#003f5f"}]
+        }, {
+          "featureType": "poi.sports_complex",
+          "elementType": "labels.text.fill",
+          "stylers": [{"color": "#003f5f"}]
+        }, {
+          "featureType": "road",
+          "elementType": "all",
+          "stylers": [{"saturation": -100}, {"lightness": 45}]
+        }, {
+          "featureType": "road.highway",
+          "elementType": "all",
+          "stylers": [{"visibility": "simplified"}]
+        }, {
+          "featureType": "road.highway",
+          "elementType": "labels.text.fill",
+          "stylers": [{"color": "#003f5f"}]
+        }, {
+          "featureType": "road.highway.controlled_access",
+          "elementType": "labels.text.fill",
+          "stylers": [{"color": "#003f5f"}]
+        }, {
+          "featureType": "road.arterial",
+          "elementType": "labels.text.fill",
+          "stylers": [{"color": "#003f5f"}]
+        }, {
+          "featureType": "road.arterial",
+          "elementType": "labels.icon",
+          "stylers": [{"visibility": "off"}]
+        }, {
+          "featureType": "road.local",
+          "elementType": "labels.text.fill",
+          "stylers": [{"color": "#438fb6"}, {"lightness": "0"}, {"gamma": "0"}, {"saturation": "0"}]
+        }, {
+          "featureType": "transit",
+          "elementType": "all",
+          "stylers": [{"visibility": "off"}]
+        }, {
+          "featureType": "water",
+          "elementType": "all",
+          "stylers": [{"color": "#328aae"}, {"visibility": "on"}]
+        }, {"featureType": "water", "elementType": "geometry.fill", "stylers": [{"color": "#003f5f"}]}]
       };
 
       this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
 
       this.addMarker();
+
+      this.loadingPopup.dismiss();
 
     }, (err) => {
       console.log(err);
@@ -50,7 +183,7 @@ export class TrackingNotRealtimePage {
 
   }
 
-  addMarker(){
+  addMarker() {
 
     let marker = new google.maps.Marker({
       map: this.map,
@@ -64,7 +197,7 @@ export class TrackingNotRealtimePage {
 
   }
 
-  addInfoWindow(marker, content){
+  addInfoWindow(marker, content) {
 
     let infoWindow = new google.maps.InfoWindow({
       content: content
