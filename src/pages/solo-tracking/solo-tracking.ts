0e75b8716d9f90770d06a98acdb732e929e2d539
Alexander van Ravestyn <alexander.vanravestyn@student.kdg.be>
2017-02-26 21:25:44 +0100

    GetLocation implementatie

--- src/pages/solo-tracking/solo-tracking.ts
+++ src/pages/solo-tracking/solo-tracking.ts
@@ -1,5 +1,8 @@
-import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import {Component, NgZone}    from '@angular/core';
+import {NavController, NavParams} from 'ionic-angular';
+import {Geolocation}  from 'ionic-native';
+import {Coordinate}   from "../../app/model/coordinate";
+import {Observable}   from "rxjs";
 
 @Component({
   selector: 'page-solotracking',
@@ -7,10 +10,64 @@ import { NavController, NavParams } from 'ionic-angular';
 })
 export class SolotrackingPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {}
+  tracking: boolean = true;
+  coordinates: Coordinate[];
+  timeInSeconds: any = 0;
+  timerDisplay: any = "00:00:00";
+
+  distance: any = 0;
+  speed: any = 0;
+  avgSpeed: any = 0;
+  avgPace: any = 0;
+
+  lat: any = "0";
+  lon: any = "0";
+
+  timer: any;
+  timerSub: any;
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, private zone: NgZone) {
+    // Start timer.
+    this.timerTick();
+
+    // Start location tracking.
+    this.timer = Observable.timer(1000, 1000);
+    this.timerSub = this.timer.subscribe(test => {
+      this.trackCoordinates();
+      this.timerTick();
+    });
+  }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad SolotrackingPage');
+  }
+
+  timerTick(): void {
+    if (this.tracking == true) {
+      this.timeInSeconds++;
+      this.timerDisplay = this.getSecondsAsDigitalClock(this.timeInSeconds);
+    }
+  }
+
+  getSecondsAsDigitalClock(inputSeconds: number): string {
+    let sec_num = parseInt(inputSeconds.toString(), 10); // don't forget the second param
+    let hours = Math.floor(sec_num / 3600);
+    let minutes = Math.floor((sec_num - (hours * 3600)) / 60);
+    let seconds = sec_num - (hours * 3600) - (minutes * 60);
+    let hoursString = (hours < 10) ? "0" + hours : hours.toString();
+    let minutesString = (minutes < 10) ? "0" + minutes : minutes.toString();
+    let secondsString = (seconds < 10) ? "0" + seconds : seconds.toString();
+    return hoursString + ':' + minutesString + ':' + secondsString;
+  }
+
+  trackCoordinates(): void {
+    Geolocation.getCurrentPosition().then((position) => {
+      this.zone.run(() => {
+        this.lat = position.coords.latitude;
+        this.lon = position.coords.longitude;
+      });
+      console.log(this.timerDisplay + ' ' + position.coords.latitude + ' ' + position.coords.longitude);
+    });
   }
 
   stopTracking(): void {
--- src/services/location/coordinate.service.ts
+++ src/services/location/coordinate.service.ts
@@ -1,6 +1,8 @@
-import { Injectable } from '@angular/core';
-import { Observer }   from 'rxjs/Observer';
-import { Observable } from 'rxjs/Observable';
+import { Injectable }   from '@angular/core';
+import { Observer }     from 'rxjs/Observer';
+import { Observable }   from 'rxjs/Observable';
+import { Geolocation }  from 'ionic-native';
+
 
 @Injectable()
 export class CoordinateService {
