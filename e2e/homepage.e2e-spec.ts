1a95d225f4255f1d8937a95f69f7a62d18e268b8
Wout Lambreghts <wout.lambreghts@student.kdg.be>
2017-02-27 08:38:02 +0100

    Homepage test aangepast werkend

--- e2e/homepage.e2e-spec.ts
+++ e2e/homepage.e2e-spec.ts
@@ -1,25 +1,27 @@
-/**
- * Created by stijnergeerts on 20/02/17.
- */
 import {by, element, browser} from "protractor";
 describe('Home page - general', () => {
   beforeEach(() => {
+    let inputMail = element.all(by.css('#maillogin'));
+    let inputPass = element.all(by.css('#passlogin'));
+    let btnLogin = element(by.id('btn-login'));
     browser.get('');
-  });
-  it('should have a title', () => {
-    expect(browser.getTitle()).toEqual('Home');
+    inputMail.all(by.tagName('input')).first().sendKeys("runtrackminds2017@gmail.com");
+    inputPass.all(by.tagName('input')).first().sendKeys("Team102017");
+
+    expect(inputMail.all(by.tagName('input')).first().getAttribute('value')).toEqual("runtrackminds2017@gmail.com");
+    expect(inputPass.all(by.tagName('input')).first().getAttribute('value')).toEqual("Team102017");
+
+    btnLogin.click().then(() => {
+      browser.driver.sleep(2000);
+    });
   });
 
-  it('should have navbar', () => {
-    let navBar = element(by.tagName('ion-navbar'));
-    expect(navBar.isPresent()).toBeTruthy();
-  });
-
-  it('test navbar title', () => {
+  //Other parts already tested in loginpage.e2e
+  it('should have a correct navbar title & button new tracking', () => {
     let navBarTitle = element.all(by.css('ion-title')).get(1);
-
     expect(navBarTitle.isPresent()).toBeTruthy();
     expect(navBarTitle.getText()).toBe('Home');
+    let btn = element(by.tagName('button'));
+    expect(btn.isPresent()).toBeTruthy();
   });
-
 });
