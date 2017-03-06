6ddeed4637e19024a74d929411f002ad6e73d422
Jens Schadron <jensschadron@hotmail.com>
2017-03-07 00:57:20 +0100

    Navigatiefouten gefixt

--- src/pages/friends/tabs/tabs.ts
+++ src/pages/friends/tabs/tabs.ts
@@ -1,10 +1,10 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
 
 import { FriendsPage } from '../../friends/friends';
 import { FriendsRequestsPage } from '../requests/friendrequests';
 import {FriendsService} from '../../../services/friends/friends.service';
 import {User} from "../../../model/user";
-import {NavController} from "ionic-angular";
+import {NavController, Tabs} from "ionic-angular";
 
 @Component({
   templateUrl: 'tabs.html',
@@ -12,15 +12,20 @@ import {NavController} from "ionic-angular";
 })
 export class TabsPage implements OnInit{
 
+  @ViewChild('tabs') tabRef: Tabs;
+
   // this tells the tabs component which Pages
   // should be each tab's root Page
-  tab1Root: any = FriendsPage;
-  tab2Root: any = FriendsRequestsPage;
-  tab3Root: any = FriendsRequestsPage;
+  friendsPage: any = FriendsPage;
+  friendRequestsPage: any = FriendsRequestsPage;
   private requests: User[] = [];
 
 
   ngOnInit(): void {
+
+  }
+
+  constructor(private FriendsService: FriendsService, public NavCtrl: NavController) {
     this.FriendsService.getFriendrequests().subscribe(
       (friends) => {
         this.requests = friends;
@@ -30,9 +35,5 @@ export class TabsPage implements OnInit{
         console.log(error as string);
       }
     );
-  }
-
-  constructor(private FriendsService: FriendsService, public NavCtrl: NavController) {
-
   }
 }
