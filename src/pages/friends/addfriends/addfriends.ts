6283430d2b88255f300da6ab6a1e939bd625d286
Jens Schadron <jensschadron@hotmail.com>
2017-03-07 17:23:23 +0100

    Kleine wijzigingen

--- src/pages/friends/addfriends/addfriends.ts
+++ src/pages/friends/addfriends/addfriends.ts
@@ -1,19 +1,18 @@
 import {Component, Inject, OnInit} from '@angular/core';
-import {AlertController} from 'ionic-angular';
-import { Platform } from 'ionic-angular';
-import { NavController }  from 'ionic-angular';
+import {AlertController, Events} from 'ionic-angular';
+import {Platform} from 'ionic-angular';
+import {NavController}  from 'ionic-angular';
 import {FriendsService} from '../../../services/friends/friends.service';
 import {User} from "../../../model/user";
 
 
-
 @Component({
     selector: 'page-addfriends',
-    templateUrl:'addfriends.html',
+    templateUrl: 'addfriends.html',
     providers: [FriendsService]
   }
 )
-export class AddFriendsPage implements OnInit{
+export class AddFriendsPage implements OnInit {
   private potentialFriends: User[] = [];
   queryString: string = "";
 
@@ -29,8 +28,10 @@ export class AddFriendsPage implements OnInit{
   }
 
   onClickAddFriend(username): void {
-    this.FriendsService.addFriend(username).subscribe(val => console.log(val), err => console.log(err));
-    setTimeout(() => this.ngOnInit(), 2000);
+    this.FriendsService.addFriend(username).subscribe(val => {
+      console.log(val);
+      this.ngOnInit()
+    }, err => console.log(err));
   }
 
 
