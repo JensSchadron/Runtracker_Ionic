31a4a3ab0d2c00bc7e1f2af6c57d1a61ca8cc4ad
Jens Schadron <jensschadron@hotmail.com>
2017-03-07 17:24:54 +0100

    aantal requests indicator in tabs wordt automatisch geupdate bij het aanvaarden of weigeren van een friend request

--- src/pages/friends/requests/friendrequests.ts
+++ src/pages/friends/requests/friendrequests.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit} from '@angular/core';
-import {App} from 'ionic-angular';
+import {App, Events} from 'ionic-angular';
 import {NavController}  from 'ionic-angular';
 import {FriendsService} from '../../../services/friends/friends.service';
 import {User} from "../../../model/user";
@@ -28,29 +28,38 @@ export class FriendsRequestsPage implements OnInit {
   onClickAddFriend(username): void {
     this.friendsService.addFriend(username).subscribe(val => {
       console.log(val);
+      this.events.publish('friendrequest:update');
       this.init();
-    }, (err) => {console.log(err)});
+    }, (err) => {
+      console.log(err)
+    });
   }
 
   onClickRemoveFriend(username): void {
     this.friendsService.deleteFriend(username).subscribe(val => {
       console.log(val);
+      this.events.publish('friendrequest:update');
       this.init();
-    }, err => console.log(err));
+    }, (err) => {
+      console.log(err);
+    });
   }
 
   onClickAcceptFriend(username): void {
     this.friendsService.acceptFriend(username).subscribe(val => {
       console.log(val);
+      this.events.publish('friendrequest:update');
       this.init();
-    }, err => console.log(err));
+    }, (err) => {
+      console.log(err);
+    });
   }
 
   onAddFriendsBtnClick(): void {
     this.appCtrl.getRootNav().push(this.addFriendsPage).then(() => console.log("Just a console log"));
   }
 
-  constructor(private friendsService: FriendsService, public navCtrl: NavController, public appCtrl: App) {
+  constructor(private friendsService: FriendsService, public navCtrl: NavController, public events: Events, public appCtrl: App) {
     this.init();
   }
 
--- src/pages/friends/tabs/tabs.ts
+++ src/pages/friends/tabs/tabs.ts
@@ -1,31 +1,37 @@
 import {Component, OnInit, ViewChild} from '@angular/core';
 
-import { FriendsPage } from '../../friends/friends';
-import { FriendsRequestsPage } from '../requests/friendrequests';
+import {FriendsPage} from '../../friends/friends';
+import {FriendsRequestsPage} from '../requests/friendrequests';
 import {FriendsService} from '../../../services/friends/friends.service';
 import {User} from "../../../model/user";
-import {NavController, Tabs} from "ionic-angular";
+import {NavController, Tabs, Events} from "ionic-angular";
 
 @Component({
   templateUrl: 'tabs.html',
   providers: [FriendsService]
 })
-export class TabsPage implements OnInit{
-
+export class TabsPage implements OnInit {
   @ViewChild('tabs') tabRef: Tabs;
 
   // this tells the tabs component which Pages
   // should be each tab's root Page
   friendsPage: any = FriendsPage;
   friendRequestsPage: any = FriendsRequestsPage;
-  private requests: User[] = [];
+  private requests: User[];
 
 
   ngOnInit(): void {
-
+    this.events.subscribe('friendrequest:update', () => {
+      console.log('event received :3');
+      this.updateFriendRequests();
+    });
   }
 
-  constructor(private FriendsService: FriendsService, public NavCtrl: NavController) {
+  constructor(private FriendsService: FriendsService, public NavCtrl: NavController, public events: Events) {
+    this.updateFriendRequests();
+  }
+
+  private updateFriendRequests(): void {
     this.FriendsService.getFriendrequests().subscribe(
       (friends) => {
         this.requests = friends;
