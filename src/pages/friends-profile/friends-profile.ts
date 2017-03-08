72a84bfea8541f64967051650f2112368d51c20c
Jens Schadron <jensschadron@hotmail.com>
2017-03-08 01:10:04 +0100

    code clean up + toevoegen Event emitter + user wordt nu automatisch teruggestuurd naar overzicht bij verwijderen van vriend

--- src/pages/friends-profile/friends-profile.ts
+++ src/pages/friends-profile/friends-profile.ts
@@ -1,16 +1,11 @@
 import {Component, OnInit} from "@angular/core";
-import {NavController, AlertController, NavParams} from "ionic-angular";
+import {NavController, NavParams, Events} from "ionic-angular";
 
 
-import {UserService} from '../../services/auth/user.service';
-import {AuthService} from '../../services/auth/auth.service';
 import {FriendProfilePageService} from '../../services/friendsprofile/friend-profilepage.service'
 import {User} from "../../model/user";
 import {CoreInfo} from "../../model/coreinfo";
 import {FriendsService} from "../../services/friends/friends.service";
-import {FriendsPage} from "../friends/friends";
-import {App} from 'ionic-angular';
-import {TabsPage} from "../friends/tabs/tabs";
 
 
 /**
@@ -19,16 +14,15 @@ import {TabsPage} from "../friends/tabs/tabs";
 @Component({
   selector: 'page-friends-profile',
   templateUrl: 'friends-profile.html',
-  providers: [FriendProfilePageService,FriendsService]
+  providers: [FriendProfilePageService, FriendsService]
 })
 export class FriendsProfilePage implements OnInit {
   private userFromParam;
-  private user = new User("","","","");
+  private user = new User("", "", "", "");
   private coreInfo;
-  private tabsPage = TabsPage;
 
   ngOnInit(): void {
-    this.userFromParam=this.navParams.data;
+    this.userFromParam = this.navParams.data;
     let username = this.userFromParam.username;
     this.friendProfilePageService.getUser(username).subscribe((user: User) => {
       this.user = user;
@@ -38,12 +32,15 @@ export class FriendsProfilePage implements OnInit {
   }
 
   onClickRemoveFriend(username): void {
+    //TODO toevoegen confirmation prompt
     this.friendsService.deleteFriend(username).subscribe(val => {
       console.log(val);
+      this.events.publish('friendrequest:update');
     }, err => console.log(err));
+    this.navCtrl.pop();
   }
 
-  constructor(public appCtrl: App,public navCtrl: NavController, public navParams: NavParams,
+  constructor(public navCtrl: NavController, public navParams: NavParams, public events: Events,
               private friendProfilePageService: FriendProfilePageService, private friendsService: FriendsService) {
 
   }
