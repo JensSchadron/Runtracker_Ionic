44693ee83d4d511ad208e09bb4f69fc89b62f5c9
Jens Schadron <jensschadron@hotmail.com>
2017-03-08 02:06:30 +0100

    MQTT packets samengevoegd

--- src/services/mqtt/packet/countdown.packet.ts
+++
@@ -1,9 +0,0 @@
-import {MQTTPacket, MQTTPacketType} from "./mqtt.packet";
-
-//TODO edit packet
-export class CountdownPacket implements MQTTPacket{
-  type: MQTTPacketType = MQTTPacketType.COUNTDOWN;
-
-  constructor() {
-  }
-}
--- src/services/mqtt/packet/mqtt.packet.ts
+++ src/services/mqtt/packet/mqtt.packet.ts
@@ -1,3 +1,4 @@
+import {Goal} from "../../../model/goal";
 export enum MQTTPacketType {
   COUNTDOWN,
   INVITE,
@@ -7,4 +8,51 @@ export enum MQTTPacketType {
 
 export interface MQTTPacket {
   type: MQTTPacketType;
+}
+
+export class InvitePacket implements MQTTPacket{
+  type: MQTTPacketType = MQTTPacketType.INVITE;
+  compId: number;
+  username: string;
+  goal: Goal;
+
+  constructor(compId: number, username: string, goal: Goal) {
+    this.compId = compId;
+    this.username = username;
+    this.goal = goal;
+  }
+}
+
+export class InviteResponsePacket implements MQTTPacket{
+  type: MQTTPacketType = MQTTPacketType.INVITE_RESPONSE;
+  compId: number;
+  userId: number;
+  accepted: boolean;
+
+  constructor(compId: number, userId: number, accepted: boolean) {
+    this.compId = compId;
+    this.userId = userId;
+    this.accepted = accepted;
+  }
+}
+
+export class ReadyPacket implements MQTTPacket{
+  type: MQTTPacketType = MQTTPacketType.READY;
+  compId: number;
+  userId: number;
+  ready: boolean;
+
+  constructor(compId: number, userId: number, ready: boolean) {
+    this.compId = compId;
+    this.userId = userId;
+    this.ready = ready;
+  }
+}
+
+//TODO edit packet
+export class CountdownPacket implements MQTTPacket{
+  type: MQTTPacketType = MQTTPacketType.COUNTDOWN;
+
+  constructor() {
+  }
 }
