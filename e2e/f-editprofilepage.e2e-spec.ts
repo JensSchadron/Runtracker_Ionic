24d5d88f8c71bda4379bd10ddda9c4fd6083129e
stijnergeerts <KdG1p7PN>
2017-03-09 15:32:36 +0100

    Tab and editprofile test

---
+++ e2e/f-editprofilepage.e2e-spec.ts
@@ -0,0 +1,45 @@
+/**
+ * Created by stijnergeerts on 20/02/17.
+ */
+import {by, element, browser} from "protractor";
+describe('EditProfile-page', () => {
+  /*beforeAll(() => {
+    browser.waitForAngularEnabled(false);
+
+    let btnMenuNavDrawer = element(by.tagName('ion-navbar')).all(by.css('button')).get(1);
+    btnMenuNavDrawer.click().then(() => {
+      browser.driver.sleep(2000);
+      let btnTabs = element(by.className('list')).all(by.tagName('button')).get(2);
+      btnTabs.click().then(() =>{
+        console.log("Profile button clicked.");
+        browser.driver.sleep(2000);
+      });
+    });
+  });
+
+
+  it('should have tabbar, tabs, header & title ', () => {
+
+    let tabBar = element(by.tagName("ion-tabs"))
+    let tabFriend= element(by.className("tab-friend"));
+    let tabRequest= element(by.className("tab-request"));
+    let title = element(by.id('tab-title'));
+    expect(tabBar.isPresent()).toBeTruthy();
+    expect(tabFriend.isPresent()).toBeTruthy();
+    expect(tabRequest.isPresent()).toBeTruthy();
+    expect(title.isPresent()).toBeTruthy();
+
+  });
+
+  afterAll(() => {
+    browser.waitForAngularEnabled(true);
+
+    let btnMenuNavDrawer = element(by.tagName('ion-navbar')).all(by.css('button')).get(1);
+    btnMenuNavDrawer.click().then(() => {
+      browser.driver.sleep(2000);
+      let btnLogout = element(by.tagName('ion-menu')).element(by.tagName('ion-list')).all(by.tagName('button')).last();
+      btnLogout.click().then(() => console.log("Log out button clicked."));
+    });
+  });*/
+
+});
