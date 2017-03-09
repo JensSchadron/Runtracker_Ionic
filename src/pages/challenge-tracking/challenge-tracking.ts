919711ab36a184662db5303b6d65c531516b7a76
Alexander van Ravestyn <alexander.vanravestyn@student.kdg.be>
2017-03-09 15:03:12 +0100

    Opzetten challenge tracking

--- src/pages/challenge-tracking/challenge-tracking.ts
+++ src/pages/challenge-tracking/challenge-tracking.ts
@@ -7,13 +7,9 @@ import {TrackingService} from "../../services/tracking/tracking.service";
 import {LocationService} from "../../services/location/location.service";
 import {CoordinateService} from "../../services/location/coordinate.service";
 import {User} from "../../model/user";
+import {Subscription} from "rxjs";
+import {Coordinate} from "../../model/coordinate";
 
-/*
- Generated class for the ChallengeTracking page.
-
- See http://ionicframework.com/docs/v2/components/#navigation for more info on
- Ionic pages and navigation.
- */
 @Component({
   selector: 'page-challenge-tracking',
   templateUrl: 'challenge-tracking.html'
@@ -22,6 +18,11 @@ import {User} from "../../model/user";
 export class ChallengeTrackingPage {
   private goalDistance: number;
   private totalDistance: number;
+
+  private coordinates: Coordinate[] = [];
+  private locationSubscription: Subscription;
+
+  private currentSpeed: number;
 
   private challengers: User[];
 
@@ -39,6 +40,50 @@ export class ChallengeTrackingPage {
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad ChallengeTrackingPage');
+    this.startTracking();
+  }
+
+  private startTracking(): void {
+    this.locationSubscription = this.locationService.receiveLocation().subscribe((position) => {
+      // Calculate distance
+      let distanceTravelled = this.calculateDistance(position.coords.latitude, position.coords.longitude);
+      this.totalDistance += distanceTravelled;
+
+      // Create new Coordinate
+      this.addNewCoordinate(position.coords.latitude, position.coords.longitude, this.currentSpeed);
+    });
+  }
+
+  private addNewCoordinate(latitude, longitude, speed) {
+    let coordinate   = new Coordinate();
+    coordinate.lat   = latitude;
+    coordinate.lon   = longitude;
+    coordinate.speed = speed;
+    coordinate.time  = Date.now().valueOf();
+    this.coordinates.push(coordinate);
+  }
+
+  private calculateSpeed(distance, currentTime): number {
+    let speed = 0;
+
+    if (this.coordinates.length > 0) {
+      let prevTime: number = this.coordinates[this.coordinates.length - 1].time;
+      speed = this.coordinateService.calculateSpeed(distance, prevTime, currentTime);
+    }
+
+    return speed;
+  }
+
+  private calculateDistance(latitude, longitude) {
+    let distance = 0;
+
+    if (this.coordinates.length > 0) {
+      let prevCoord: Coordinate = this.coordinates[this.coordinates.length - 1];
+      distance = this.coordinateService.calculateDistanceBetweenCoordinates(latitude, longitude, prevCoord.lat, prevCoord.lon);
+    }
+
+    console.log("Total distance in km added: " + distance);
+    return distance;
   }
 
 }
