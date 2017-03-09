036ad660ecb5ee15c386e3441033a4f1e9c9f515
Jens Schadron <jensschadron@hotmail.com>
2017-03-09 14:50:58 +0100

    wijzigingen challenge tracking

--- src/pages/challenge-tracking/challenge-tracking.ts
+++ src/pages/challenge-tracking/challenge-tracking.ts
@@ -1,19 +1,40 @@
-import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import {Component} from '@angular/core';
+import {NavController, NavParams} from 'ionic-angular';
+import {AuthHttpImpl} from "../../services/auth/auth-http-impl";
+import {MQTTService} from "../../services/mqtt/mqtt.service";
+import {UserService} from "../../services/auth/user.service";
+import {TrackingService} from "../../services/tracking/tracking.service";
+import {LocationService} from "../../services/location/location.service";
+import {CoordinateService} from "../../services/location/coordinate.service";
+import {User} from "../../model/user";
 
 /*
-  Generated class for the ChallengeTracking page.
+ Generated class for the ChallengeTracking page.
 
-  See http://ionicframework.com/docs/v2/components/#navigation for more info on
-  Ionic pages and navigation.
-*/
+ See http://ionicframework.com/docs/v2/components/#navigation for more info on
+ Ionic pages and navigation.
+ */
 @Component({
   selector: 'page-challenge-tracking',
   templateUrl: 'challenge-tracking.html'
 })
 export class ChallengeTrackingPage {
+  private goalDistance: number;
+  private totalDistance: number;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {}
+  private challengers: User[];
+
+  constructor(public navCtrl: NavController,
+              public navParams: NavParams,
+              public authHttp: AuthHttpImpl,
+              public mqttService: MQTTService,
+              private userService: UserService,
+              private coordinateService: CoordinateService,
+              private locationService: LocationService,
+              public trackingService: TrackingService) {
+    this.goalDistance = this.navParams.get('goalDistance');
+    this.challengers = this.navParams.get('users');
+  }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad ChallengeTrackingPage');
