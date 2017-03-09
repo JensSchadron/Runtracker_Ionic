5260932378f73f8f4da6c6339d59d0350cf55a4b
Wout Lambreghts <wout.lambreghts@student.kdg.be>
2017-03-09 16:13:23 +0100

    Test edit profile

--- e2e/f-editprofilepage.e2e-spec.ts
+++ e2e/f-editprofilepage.e2e-spec.ts
@@ -1,33 +1,49 @@
-/**
- * Created by stijnergeerts on 20/02/17.
- */
+
 import {by, element, browser} from "protractor";
 describe('EditProfile-page', () => {
-  /*beforeAll(() => {
+  beforeAll(() => {
     browser.waitForAngularEnabled(false);
+
+    let inputMail = element.all(by.css('#maillogin'));
+    let inputPass = element.all(by.css('#passlogin'));
+    let btnLogin = element(by.id('btn-login'));
+    browser.get('');
+    inputMail.all(by.tagName('input')).first().sendKeys("runtrackminds2017@gmail.com");
+    inputPass.all(by.tagName('input')).first().sendKeys("Team102017");
+
+    expect(inputMail.all(by.tagName('input')).first().getAttribute('value')).toEqual("runtrackminds2017@gmail.com");
+    expect(inputPass.all(by.tagName('input')).first().getAttribute('value')).toEqual("Team102017");
+
+    btnLogin.click().then(() => {
+      browser.driver.sleep(10000);
+    });
+
 
     let btnMenuNavDrawer = element(by.tagName('ion-navbar')).all(by.css('button')).get(1);
     btnMenuNavDrawer.click().then(() => {
       browser.driver.sleep(2000);
-      let btnTabs = element(by.className('list')).all(by.tagName('button')).get(2);
-      btnTabs.click().then(() =>{
+      let btnEditProf= element(by.className('list')).all(by.tagName('button')).get(3);
+      btnEditProf.click().then(() =>{
         console.log("Profile button clicked.");
-        browser.driver.sleep(2000);
+        browser.driver.sleep(10000);
       });
     });
+
   });
 
 
-  it('should have tabbar, tabs, header & title ', () => {
+  it('should have ion-avatar,core-info, title & save changes button', () => {
 
-    let tabBar = element(by.tagName("ion-tabs"))
-    let tabFriend= element(by.className("tab-friend"));
-    let tabRequest= element(by.className("tab-request"));
-    let title = element(by.id('tab-title'));
-    expect(tabBar.isPresent()).toBeTruthy();
-    expect(tabFriend.isPresent()).toBeTruthy();
-    expect(tabRequest.isPresent()).toBeTruthy();
+    let saveChanges = element(by.id("save-changes"));
+    let avatar= element(by.id("avatar"));
+    let title = element(by.tagName("ion-title"));
+    let coreInfo= element(by.id("core-info"));
+
+    expect(saveChanges.isPresent()).toBeTruthy();
+    expect(avatar.isPresent()).toBeTruthy();
+    expect(coreInfo.isPresent()).toBeTruthy();
     expect(title.isPresent()).toBeTruthy();
+    //expect(title.getText()).toBe('Edit profile');
 
   });
 
@@ -40,6 +56,6 @@ describe('EditProfile-page', () => {
       let btnLogout = element(by.tagName('ion-menu')).element(by.tagName('ion-list')).all(by.tagName('button')).last();
       btnLogout.click().then(() => console.log("Log out button clicked."));
     });
-  });*/
+  });
 
 });
